38030ff0739edd2f1f191d28adef664fea87d2e1
Andrew Crosio <andrew.crosio@gmail.com>
2013-06-19 09:59:32 +0400

    master: initial commit

---
+++ get_times.py
@@ -0,0 +1,33 @@
+import sys
+
+
+FILENAME = '.nose-stopwatch-times'
+
+
+def get_times(threshold=0.0):
+    with open(FILENAME, 'r') as time_file:
+        info = time_file.readlines()[1:]
+
+    for pos in xrange(0, len(info) - 1, 3):
+        name_info = info[pos][3:-2]
+        time_info = float(info[pos+2][1:])
+        if time_info >= threshold:
+            yield (name_info, time_info)
+
+
+def print_times(threshold=0.0):
+    for name, time in get_times(threshold):
+        print '%s: %.6f' % (name, time)
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        threshold = 0.0
+    elif len(sys.argv) == 2:
+        threshold = float(sys.argv[1])
+    else:
+       print 'Usage: %s [treshold_time_as_float]'
+       sys.exit(1)
+
+    print_times(threshold)
